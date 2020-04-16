e0525e004b8c755c5fe0ebabb12ceb5f0c20c25c
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-04-15 22:47:16 -0300

    fixed css2

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./index.css";
 
-import { GAME_SIZE } from "../../settings/constants";
+import { TILE_SIZE } from "../../settings/constants";
 
 const Hero = () => {
   return (
     <div
       style={{
         position: "absolute",
-        bottom: 0,
-        left: 0,
-        width: GAME_SIZE,
-        height: GAME_SIZE,
+        bottom: 48 * 2,
+        left: 48 * 2,
+        width: TILE_SIZE,
+        height: TILE_SIZE,
         backgroundImage: "url(./assets/HERO.png)",
         backgroundRepeat: "no-repeat",
         animation: "hero-animation 1s steps(4) infinite",
